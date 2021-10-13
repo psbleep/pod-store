f756aae56b530058b156baa931236be9e85c212f
Patrick Schneeweis <psbleep@protonmail.com>
2021-10-13 15:27:11 -0700

    make store_file_path attribute public

--- pod_store/store_file_handlers.py
+++ pod_store/store_file_handlers.py
@@ -8,7 +8,7 @@ from .exc import GPGCommandError
 
 class StoreFileHandler(ABC):
     def __init__(self, store_file_path: str):
-        self._store_file_path = store_file_path
+        self.store_file_path = store_file_path
 
     @classmethod
     def create_store_file(cls, store_file_path: str, store_data: dict = None, **kwargs):
@@ -28,36 +28,41 @@ class StoreFileHandler(ABC):
 
 
 class EncryptedStoreFileHandler(StoreFileHandler):
+    """Class for reading/writing data from an encrypted store file.
+
+    store_file_path (str): file system location of the json file that holds store data.
+    """
+
     def __init__(self, gpg_id: str, *args, **kwargs):
         self._gpg_id = gpg_id
         super().__init__(*args, **kwargs)
 
     def __repr__(self):
-        return f"<EncryptedStoreFileHandler({self._store_file_path!r})>"
+        return f"<EncryptedStoreFileHandler({self.store_file_path!r})>"
 
     def read_data(self):
         """Retrieve encrypted json data from the store file."""
 
-        cmd = f"gpg -d {self._store_file_path}"
+        cmd = f"gpg -d {self.store_file_path}"
         decrypted = self._run_gpg_command(cmd)
         return json.loads(decrypted)
 
     def write_data(self, data: dict):
         """Write encrypted json data to the store file."""
         try:
-            with open(self._store_file_path, "rb") as f:
+            with open(self.store_file_path, "rb") as f:
                 existing_data = f.read()
-            os.remove(self._store_file_path)
+            os.remove(self.store_file_path)
         except FileNotFoundError:
             existing_data = b""
 
-        tmp_file = os.path.join(os.path.dirname(self._store_file_path), ".tmp")
+        tmp_file = os.path.join(os.path.dirname(self.store_file_path), ".tmp")
         with open(tmp_file, "w") as f:
             json.dump(data, f)
 
         try:
             cmd = (
-                f"gpg --output {self._store_file_path} "
+                f"gpg --output {self.store_file_path} "
                 "--encrypt "
                 f"--recipient {self._gpg_id} "
                 f"{tmp_file}"
@@ -65,7 +70,7 @@ class EncryptedStoreFileHandler(StoreFileHandler):
             self._run_gpg_command(cmd)
         except Exception as err:
             os.remove(tmp_file)
-            with open(self._store_file_path, "wb") as f:
+            with open(self.store_file_path, "wb") as f:
                 f.write(existing_data)
             raise GPGCommandError(str(err))
 
@@ -80,18 +85,18 @@ class EncryptedStoreFileHandler(StoreFileHandler):
 class UnencryptedStoreFileHandler(StoreFileHandler):
     """Class for reading/writing data from an unencrypted store file.
 
-    _store_file_path (str): file system location of the json file that holds store data.
+    store_file_path (str): file system location of the json file that holds store data.
     """
 
     def __repr__(self):
-        return f"<UnencryptedStoreFileHandler({self._store_file_path!r})>"
+        return f"<UnencryptedStoreFileHandler({self.store_file_path!r})>"
 
     def read_data(self):
         """Retrieve unencrypted json data from the store file."""
-        with open(self._store_file_path) as f:
+        with open(self.store_file_path) as f:
             return json.load(f)
 
     def write_data(self, data: dict):
         """Write unencrypted json data to the store file."""
-        with open(self._store_file_path, "w") as f:
+        with open(self.store_file_path, "w") as f:
             json.dump(data, f, indent=2)
