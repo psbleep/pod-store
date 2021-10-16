41bc9eed258e8d3c5ca3175744fe1942deded8a7
Patrick Schneeweis <psbleep@protonmail.com>
2021-10-16 06:38:33 -0700

    add get_episodes command helper

--- pod_store/commands/helpers.py
+++ pod_store/commands/helpers.py
@@ -2,6 +2,7 @@ from typing import Any, List, Optional
 
 import click
 
+from ..episodes import Episode
 from ..podcasts import Podcast
 from ..store import Store
 
@@ -10,6 +11,35 @@ def abort_if_false(ctx: click.Context, _, value: Any):
     """Callback for aborting a Click command from within an argument or option."""
     if not value:
         ctx.abort()
+
+
+def get_episodes(
+    store: Store,
+    new: Optional[bool] = None,
+    podcast_title: Optional[str] = None,
+    allow_empty: bool = False,
+) -> List[Episode]:
+    """Helper method for filtering a list of episodes in the store from cli args.
+
+    Builds the filters used by the `pod_store.Podcasts.PodcastEpisodes.list` method.
+
+    If no podcast title is specified, will look through episodes for all store podcasts.
+    """
+    podcast_filters = {}
+    if podcast_title:
+        podcast_filters["title"] = podcast_title
+    if new:
+        podcast_filters["has_new_episodes"] = True
+
+    episode_filters = {}
+    if new:
+        episode_filters = {"downloaded_at": None}
+
+    podcasts = store.podcasts.list(allow_empty=allow_empty, **podcast_filters)
+    episodes = []
+    for pod in podcasts:
+        episodes.extend(pod.episodes.list(allow_empty=allow_empty, **episode_filters))
+    return episodes
 
 
 def get_podcasts(
